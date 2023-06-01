2d6bc70038982b8706309b4b169d94e96c617c34
prefer_choi <prefer_choi@naver.com>
2023-06-01 09:48:14 +0900

    init:jwt

--- jwt/node/app.js
+++ jwt/node/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const jwt = require('jsonwebtoken');
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });

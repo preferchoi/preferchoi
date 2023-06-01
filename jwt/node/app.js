63e2140d16901921cfb76fd59cf691396a629139
prefer_choi <prefer_choi@naver.com>
2023-06-01 11:13:30 +0900

    feat:jwt_login

--- jwt/node/app.js
+++ jwt/node/app.js
@@ -2,10 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const jwt = require('jsonwebtoken');
+
+const users = [
+  { id: 1, username: 'test', password: 'test' },
+  // You can add more users here
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username && u.password === password);
+  if (user == null) {
+    return res.status(400).send('Invalid username or password');
+  }
+
+  const token = jwt.sign({ id: user.id, username: user.username }, 'secret-key', { expiresIn: '1h' });
+  res.send({ token });
 });
 
 app.listen(port, () => {

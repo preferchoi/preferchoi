ef76b40d6cb654ccd0a18face2474c7b2843480d
prefer_choi <prefer_choi@naver.com>
2023-06-02 15:40:26 +0900

    feat:login_check

--- jwt/node/app.js
+++ jwt/node/app.js
@@ -27,8 +27,33 @@ app.post('/login', (req, res) => {
   }
 
   const token = jwt.sign({ id: user.id, username: user.username }, 'secret-key', { expiresIn: '1h' });
+  // 인증 토큰 전송
   res.send({ token });
 });
+
+
+app.get('/login_check', verifyToken, (req, res) => {
+  res.send(req.user);
+});
+
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (token == null) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  jwt.verify(token, 'secret-key', (err, user) => {
+    if (err) {
+      return res.status(403).send('Invalid token');
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);

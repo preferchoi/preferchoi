c46f882fb6f1937fb1b6095c2ebf5a9c774f2d6b
prefer_choi <prefer_choi@naver.com>
2023-11-22 14:18:57 +0900

    feat: 커서 기반 페이지네이션 구현

--- graphql/server/src/resolvers/Film.ts
+++ graphql/server/src/resolvers/Film.ts
@@ -1,18 +1,62 @@
-import { Query, Resolver, FieldResolver, Root } from "type-graphql";
-import ghibilData from "../data/ghibli";
-import { Film } from "../entities/Film";
-import { Director } from "../entities/director";
+import {
+  Query,
+  Resolver,
+  FieldResolver,
+  Root,
+  ObjectType,
+  Field,
+  Int,
+  Arg,
+} from 'type-graphql';
+import ghibliData from '../data/ghibli';
+import { Film } from '../entities/Film';
+import { Director } from '../entities/Director';
+
+@ObjectType()
+class PaginatedFilms {
+  @Field(() => [Film])
+  films: Film[];
+
+  @Field(() => Int, { nullable: true })
+  cursor?: Film['id'] | null;
+}
 
 @Resolver(Film)
 export class FilmResolver {
-  @Query(() => [Film])
-  films(): Film[] {
-    return ghibilData.films;
+  @Query(() => PaginatedFilms)
+  films(
+    @Arg('limit', () => Int, { nullable: true, defaultValue: 6 }) limit: number,
+    @Arg('cursor', () => Int, { nullable: true, defaultValue: 1 })
+    cursor: Film['id'],
+  ): PaginatedFilms {
+    const realLimit = Math.min(6, limit);
+
+    if (!cursor) return { films: [] };
+
+    const cursorDataIndex = ghibliData.films.findIndex((f) => f.id === cursor);
+
+    if (cursorDataIndex === -1) return { films: [] };
+
+    const result = ghibliData.films.slice(
+      cursorDataIndex,
+      cursorDataIndex + realLimit,
+    );
+
+    const nextCursor = result[result.length - 1].id + 1;
+    const hasNext = ghibliData.films.findIndex((f) => f.id === nextCursor) > -1;
+
+    return {
+      cursor: hasNext ? nextCursor : null,
+      films: result,
+    };
   }
-  
   @FieldResolver(() => Director)
   director(@Root() parentFilm: Film): Director | undefined {
-    return ghibilData.directors.find((dr) => dr.id === parentFilm.director_id);
+    return ghibliData.directors.find((dr) => dr.id === parentFilm.director_id);
   }
-  
+
+  @Query(() => Film, { nullable: true })
+  film(@Arg('filmId', () => Int) filmId: number): Film | undefined {
+    return ghibliData.films.find((x) => x.id === filmId);
+  }
 }

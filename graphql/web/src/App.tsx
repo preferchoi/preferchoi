ef482637e2d821613a20463c382ad9bc7d38ce2c
prefer_choi <prefer_choi@naver.com>
2023-11-22 15:04:58 +0900

    refactor: 아폴로 클라이언트 코드 분리

--- graphql/web/src/App.tsx
+++ graphql/web/src/App.tsx
@@ -1,34 +1,10 @@
 import * as React from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react';
 import FilmList from './compnents/film/FilmList';
-import { PaginatedFilms } from './generated/graphql';
+import { createApolloClient } from "./apollo/createApolloClient";
 
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          films: {
-            keyArgs: false,
-            merge: (
-              existing: PaginatedFilms | undefined,
-              incoming: PaginatedFilms,
-            ): PaginatedFilms => {
-              return {
-                cursor: incoming.cursor,
-                films: existing
-                  ? [...existing.films, ...incoming.films]
-                  : incoming.films,
-              };
-            },
-          },
-        },
-      },
-    },
-  }),
-});
+const apolloClient = createApolloClient()
 
 export const App = () => (
   <ApolloProvider client={apolloClient}>
---
+++ graphql/web/src/apollo/createApolloClient.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+import { createApolloCache } from "./createApolloCache";
+
+export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: createApolloCache(),
+});

2e344c4507e585ae391da3a794f1f4c25c4e2b8f
prefer_choi <prefer_choi@naver.com>
2023-11-22 11:47:38 +0900

    chakra ui 적용

--- graphql/web/src/compnents/film/FilmList.tsx
+++ graphql/web/src/compnents/film/FilmList.tsx
@@ -1,10 +1,23 @@
 import { useFilmsQuery } from '../../generated/graphql';
+import { Box, SimpleGrid, Skeleton } from '@chakra-ui/react';
+import FIlmCard from './FIlmCard';
 
 export default function FilmList(): JSX.Element {
   const { data, loading, error } = useFilmsQuery();
 
-  if (loading) return <p>...loading </p>;
   if (error) return <p>{error.message}</p>;
-  
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+
+  return (
+    <SimpleGrid columns={[2, null, 3]} spacing={[2, null, 10]}>
+      {loading &&
+        new Array(6).fill(0).map((x) => <Skeleton key={x} height={'400px'} />)}
+      {!loading &&
+        data &&
+        data.films.map((film) => (
+          <Box key={film.id}>
+            <FIlmCard film={film} />
+          </Box>
+        ))}
+    </SimpleGrid>
+  );
 }

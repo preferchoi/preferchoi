2385eb204f7991a970a98880b28af16f0d172e62
prefer_choi <prefer_choi@naver.com>
2023-11-22 14:51:11 +0900

    feat: 아폴로 클라이언트에서 캐시 사용

--- graphql/web/src/App.tsx
+++ graphql/web/src/App.tsx
@@ -1,27 +1,42 @@
-import * as React from "react"
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  theme,
-} from "@chakra-ui/react"
-import FilmList from "./compnents/film/FilmList"
+import * as React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react';
+import FilmList from './compnents/film/FilmList';
+import { PaginatedFilms } from './generated/graphql';
 
 const apolloClient = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-})
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          films: {
+            keyArgs: false,
+            merge: (
+              existing: PaginatedFilms | undefined,
+              incoming: PaginatedFilms,
+            ): PaginatedFilms => {
+              return {
+                cursor: incoming.cursor,
+                films: existing
+                  ? [...existing.films, ...incoming.films]
+                  : incoming.films,
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
+});
 
 export const App = () => (
   <ApolloProvider client={apolloClient}>
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Text>
-          Edit  and save to reload.
-        </Text>
+        <Text>Edit and save to reload.</Text>
         <FilmList />
       </Box>
     </ChakraProvider>
   </ApolloProvider>
-)
+);

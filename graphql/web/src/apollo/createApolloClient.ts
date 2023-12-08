c752132c2c9b54682a076ed98d4f3d48229dfea3
prefer_choi <prefer_choi@naver.com>
2023-12-08 22:28:12 +0900

    feat: 토큰 만료 시 자동 새로고침 요청 전송

--- graphql/web/src/apollo/createApolloClient.ts
+++ graphql/web/src/apollo/createApolloClient.ts
@@ -4,27 +4,41 @@ import {
   from,
   HttpLink,
   operationName,
+  fromPromise,
 } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import { setContext } from '@apollo/client/link/context';
 import { createApolloCache } from './createApolloCache';
+import { refreshAccessToken } from './auth';
+let apolloClient: ApolloClient<NormalizedCacheObject>;
 
-const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      // eslint-disable-next-line no-console
-      console.log(`[GraphQL error]: -> ${operationName}
+const errorLink = onError(
+  // eslint-disable-next-line consistent-return
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      if (
+        graphQLErrors.find((error) => error.message === 'access token expired')
+      ) {
+        return fromPromise(refreshAccessToken(apolloClient, operation))
+          .filter((result) => !!result)
+          .flatMap(() => forward(operation));
+      }
+
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        // eslint-disable-next-line no-console
+        console.log(`[GraphQL error]: -> ${operationName}
       Message: ${message}, Query: ${path}, Location: ${JSON.stringify(
         locations,
       )}`),
-    );
-  }
-  if (networkError) {
-    // eslint-disable-next-line no-console
-    console.log(`[networkError]: -> ${operation.operationName}
+      );
+    }
+    if (networkError) {
+      // eslint-disable-next-line no-console
+      console.log(`[networkError]: -> ${operation.operationName}
     Message: ${networkError.message}`);
-  }
-});
+    }
+  },
+);
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',

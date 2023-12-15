96290e329d5e5d8b1cf8672a180b3153a75a1807
prefer_choi <prefer_choi@naver.com>
2023-12-15 13:34:47 +0900

    feat: 좋아요 뮤테이션

--- graphql/server/src/resolvers/Cut.ts
+++ graphql/server/src/resolvers/Cut.ts
@@ -1,7 +1,20 @@
-import { Arg, FieldResolver, Int, Query, Resolver, Root } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  FieldResolver,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+  UseMiddleware,
+} from 'type-graphql';
 import { Cut } from '../entities/Cut';
 import { Film } from '../entities/Film';
 import ghibliData from '../data/ghibli';
+import { isAuthenticated } from '../middelwarers/isAuthenticated';
+import { MyContext } from 'src/apollo/createApolloServer';
+import { CutVote } from 'src/entities/CutVote';
 
 @Resolver(Cut)
 export class CutResolver {
@@ -18,5 +31,25 @@ export class CutResolver {
   @FieldResolver(() => Film, { nullable: true })
   film(@Root() cut: Cut): Film | undefined {
     return ghibliData.films.find((film) => film.id === cut.filmId);
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuthenticated)
+  async vote(
+    @Arg('cutId', () => Int) cutId: number,
+    @Ctx() { verifiedUser }: MyContext,
+  ): Promise<boolean> {
+    if (verifiedUser) {
+      const { userId } = verifiedUser;
+      const alreadyVote = await CutVote.findOne({ where: { cutId, userId } });
+      if (alreadyVote) {
+        await alreadyVote.remove();
+        return true;
+      }
+      const vote = CutVote.create({ cutId, userId });
+      await vote.save();
+      return true;
+    }
+    return false
   }
 }
